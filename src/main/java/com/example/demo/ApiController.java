f85c1ea3648a89716cc28b8d7c1f83cce0c795e2
pr0cos <fedotglazko17@gmail.com>
2023-12-21 10:36:45 +0300

    4 commit

--- src/main/java/com/example/demo/ApiController.java
+++ src/main/java/com/example/demo/ApiController.java
@@ -50,4 +50,27 @@ public class ApiController {
         books.clear();
         return ResponseEntity.accepted().build();
     }
+
+    @PostMapping("book/{index}/review") //curl -X POST -H "Content-Type: text/plain" -d "Хорошая книга!" localhost:8080/book/0/review
+    public ResponseEntity<Void> newReview(@PathVariable("index") Integer id, @RequestBody String review){
+        books.get(id).newReview(review);
+        return ResponseEntity.accepted().build();
+    }
+
+    @DeleteMapping("book/{index1}/review/{index2}") //curl -X DELETE localhost:8080/book/0/review/0
+    public ResponseEntity<Void> deleteReview(@PathVariable("index1") Integer id1, @PathVariable("index2") Integer id2){
+        books.get(id1).deleteReview(id2);
+        return ResponseEntity.accepted().build();
+    }
+
+    @PutMapping("book/{index1}/review/{index2}") //curl -X PUT -H "Content-Type: text/plain" -d "Очень хорошая книга!" localhost:8080/book/0/review/0
+    public ResponseEntity<Void> changeReview(@PathVariable("index1") Integer id1, @PathVariable("index2") Integer id2, @RequestBody String review){
+        books.get(id1).changeReview(id2, review);
+        return ResponseEntity.accepted().build();
+    }
+
+    @GetMapping("book/{index}/review") //curl -X GET localhost:8080/book/0/review
+    public ResponseEntity<List<String>> getReviews(@PathVariable("index") Integer id){
+        return ResponseEntity.ok(books.get(id).getReview());
+    }
 }
--- src/main/java/com/example/demo/Book.java
+++ src/main/java/com/example/demo/Book.java
@@ -1,11 +1,16 @@
 package com.example.demo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Book {
 
     String name;
+    List<String> review;
 
     public Book(String name) {
         this.name = name;
+        this.review = new ArrayList<>();
     }
 
     public String getName() {
@@ -14,5 +19,21 @@ public class Book {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<String> getReview() {
+        return review;
+    }
+
+    public void newReview(String comment){
+        review.add(comment);
+    }
+
+    public void deleteReview(int id){
+        review.remove(id);
+    }
+
+    public void changeReview(int id, String comment){
+        review.set(id, comment);
     }
 }
